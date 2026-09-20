90e2dea17518bfa52ba66c8079da1a94b6b1b13d
md-anas-ali <anas015987@gmail.com>
2026-09-20 16:54:37 +0600

    Add files via upload

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -389,7 +389,7 @@ runLoop:
 			}
 			result.Status = model.StatusError
 			result.Error = rc.Redactor.RedactString(nodeErr.Error())
-			if node.ContinueOnFail && !IsFatal(nodeErr) {
+			if node.ContinueOnFail {
 				errItem := model.Item{JSON: map[string]any{"error": nodeErr.Error()}}
 				errOut := model.NodeOutput{{errItem}}
 				branchIdx := 0
